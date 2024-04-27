f80e5e40c62c744441bdbbeb4975597f0b124ded
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-04-26 21:37:05 -0500

    major progress to questions and generating the markdown

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -17,7 +18,40 @@ const questions = [
     {
         type: 'input',
         name: 'status',
-        message: 'What is the status of your project?'        
+        message: 'What is the status of your project?',
+        filter: function trimStatus(status) {
+            return status.trim();
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'Please input a link to the GitHub repository of your project.',
+        default: 'N/A'
+    },
+    {
+        type: 'input',
+        name: 'site',
+        message: 'Please input a link to the live deployment of your application.',
+        default: 'N/A'       
+    },
+    {
+        type: 'input',
+        name: 'media',
+        message: 'Please input any links to screenshots or video of your project, separating each with a comma, or if there are none, press enter or enter "N/A".',
+        default: 'N/A',
+        filter: function(value) {
+            return value.split(',').map(link => link.trim());
+        }
+    },
+    {
+        type: 'input',
+        name: 'alt',
+        message: 'Please input any alt text for your media, separating each with a comma, or if there is none, press enter or enter "N/A".',
+        default: 'N/A',
+        filter: function(value) {
+            return value.split(',').map(alt => alt.trim());
+        }   
     },
     {
         type: 'input',
@@ -37,20 +71,60 @@ const questions = [
     {
         type: 'input',
         name: 'credits',
-        message: 'Please list any collaborators for the project and any attributions for third-party applications or assets.'
+        message: 'Please list any collaborators for the project and any attributions for third-party applications or assets, separating each with a comma.',
+        filter: function(value) {
+            return value.split(',').map(credit => credit.trim());
+        }
     },
     {
         type: 'input',
         name: 'license',
         message: 'What license is your project under?'        
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can other users contribute to the project?'        
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions for the project?'        
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'        
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What email can people contact you at?'        
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (error) => {
+        if (error) {
+            console.error('There was an error writing the README file: ', error);
+        } else {
+            console.log('README file was successfully created');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log('Answers: ', response);
+            writeToFile('README.md', response);
+        })
+        .catch((error) => {
+            console.error('An error occurred: ', error);
+        });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,9 +11,103 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// Converting the project status to a slug if it includes spaces
+function convertStatusToSlug(data) {
+  const convertedStatus = data.status.replace(/\s+/g, '-').toLowerCase();
+  return convertedStatus
+}
 
+// Converting our media and alt values to a variable that allows us to display however many links are included
+let mediaMarkdown = '';
+if (data.link[0] !== 'N/A') {
+  for (let i = 0; i < data.link.length; i++) {
+    mediaMarkdown += `![${data.alt[i]}](${data.link[i]})\n\n`
+  }
+}
+
+let tableOfContentsStart = `
+## Table of Contents
+
+[Project Status](#project-status)
+`;
+
+let tableOfContentsEnd = `
+\n[Purpose](#purpose)
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Credits](#credits)
+
+[License](#license)
+
+[Contributing](#contributing)
+
+[Tests](#tests)
+
+[Questions](#questions)
+`
+if (data.link[0] !== 'N/A') {
+  tableOfContentsStart += `\n[Media of Application](#media-of-${convertStatusToSlug(data)}-application)`;
+}
+
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
+
+  ### ${data.description}
+
+  ${tableOfContentsStart}
+  ${tableOfContentsEnd}
+
+  ## Project Status
+
+  **${data.status}**
+
+  [GitHub Repository](${data.repo})
+
+  [Live ${data.title}](${data.site})
+
+  ${data.link[0] !== 'N/A' ? `
+  ## Media of ${data.status} Application
+
+  ${mediaMarkdown}` : ''}
+
+  ## Purpose
+
+  ${data.purpose}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits[n]}
+
+  ## License
+
+  ${license}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  Please reach out to me with any questions:
+
+  https://github.com/${data.github}/
+  ${data.email}
 `;
 }
 
