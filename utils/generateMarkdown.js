76e6416864d229776d66b2783c29b980bf601d25
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-04-27 21:28:26 -0500

    we have a functioning product! need to work on badges next

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,28 +11,30 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-// Converting the project status to a slug if it includes spaces
-function convertStatusToSlug(data) {
-  const convertedStatus = data.status.replace(/\s+/g, '-').toLowerCase();
-  return convertedStatus
-}
+function generateMarkdown(data) {
+  console.log('entire media', data.media)
+  console.log('media index 0', data.media[0])
 
-// Converting our media and alt values to a variable that allows us to display however many links are included
-let mediaMarkdown = '';
-if (data.link[0] !== 'N/A') {
-  for (let i = 0; i < data.link.length; i++) {
-    mediaMarkdown += `![${data.alt[i]}](${data.link[i]})\n\n`
+  let mediaMarkdown = '';
+  if (data.media[0] !== 'N/A') {
+    for (let i = 0; i < data.media.length; i++) {
+      mediaMarkdown += `![${data.alt[i]}](${data.media[i]})\n\n`
+    }
   }
-}
 
-let tableOfContentsStart = `
+  let creditsMarkdown = '';
+  for (let i = 0; i < data.credits.length; i++) {
+    creditsMarkdown += `* ${data.credits[i]}\n`
+  }
+
+  let tableOfContentsStart = `
 ## Table of Contents
 
 [Project Status](#project-status)
 `;
 
 let tableOfContentsEnd = `
-\n[Purpose](#purpose)
+[Purpose](#purpose)
 
 [Installation](#installation)
 
@@ -48,67 +50,67 @@ let tableOfContentsEnd = `
 
 [Questions](#questions)
 `
-if (data.link[0] !== 'N/A') {
-  tableOfContentsStart += `\n[Media of Application](#media-of-${convertStatusToSlug(data)}-application)`;
-}
 
-function generateMarkdown(data) {
+  if (data.media[0] !== 'N/A') {
+    tableOfContentsStart += `\n[Media of Application](#media-for-application)`;
+  }
+
   return `
-  # ${data.title}
+# ${data.title}
 
-  ### ${data.description}
+### ${data.description}
 
-  ${tableOfContentsStart}
-  ${tableOfContentsEnd}
+${tableOfContentsStart}
+${tableOfContentsEnd}
 
-  ## Project Status
+## Project Status
 
-  **${data.status}**
+**${data.status}**
 
-  [GitHub Repository](${data.repo})
+[GitHub Repository](${data.repo})
 
-  [Live ${data.title}](${data.site})
+[Live ${data.title}](${data.site})
 
-  ${data.link[0] !== 'N/A' ? `
-  ## Media of ${data.status} Application
+${data.media[0] !== 'N/A' ? `
+## Media for Application
 
-  ${mediaMarkdown}` : ''}
+${mediaMarkdown}` : ''}
 
-  ## Purpose
+## Purpose
 
-  ${data.purpose}
+${data.purpose}
 
-  ## Installation
+## Installation
 
-  ${data.installation}
+${data.installation}
 
-  ## Usage
+## Usage
 
-  ${data.usage}
+${data.usage}
 
-  ## Credits
+## Credits
 
-  ${data.credits[n]}
+${creditsMarkdown}
 
-  ## License
+## License
 
-  ${license}
+${data.license}
 
-  ## Contributing
+## Contributing
 
-  ${data.contributing}
+${data.contributing}
 
-  ## Tests
+## Tests
 
-  ${data.tests}
+${data.tests}
 
-  ## Questions
+## Questions
 
-  Please reach out to me with any questions:
+Please reach out to me with any questions:
 
-  https://github.com/${data.github}/
-  ${data.email}
-`;
+- Github: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
